01d9ac9abf3893d297dab3b7106ce05d6844638b
oceanxuhaiao <oceanxuhaiao@qq.com>
2026-08-29 06:28:35 +0000

    Deploying to gh-pages from @ oceanxuhaiao/milkonomy-DG@4bb60803c30869e3fc4192e26e0b39276ef9f8c2 🚀

---
+++ assets/index-BunfdHIl.js
@@ -0,0 +1,1 @@
+import{L as o,v as a}from"./directive-CJrWYquw.js";const n={install(i){i.directive("loading",a),i.config.globalProperties.$loading=o},directive:a,service:o};export{n as E};
---
+++ assets/validator-C51F6ahh.js
@@ -0,0 +1,1 @@
+import{bH as i}from"./index-B4DMY8Cs.js";const n=o=>["",...i].includes(o);export{n as i};
